929565a41abd6afb432ee7b324adb65bef047e54
Kyle <kylehendricks0707@gmail.com>
2022-05-20 09:53:17 -0400

    sample tests

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(a, b):
+    """Add a and b and return result."""
+    return a + b
+
+
+def subtract(a, b):
+    """Subtract b from s and return result"""
+    return a - b
